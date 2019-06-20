058b0d1d1dbaaff195d78c7339647075a9c2e6c0
yaling.xu <yaling.xu@appshahe.com>
2019-06-20 17:54:25 +0800

    测maven-docker + git.commit.id

---
+++ src/main/java/com/metaapp/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.metaapp.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Mloong on 2019/03/05.
+ */
+@ComponentScan("com.metaapp")
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
